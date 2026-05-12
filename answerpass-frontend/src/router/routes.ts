5eb1aad53ae36d322f8088f17f001fba1ef27c0d
沈笙 <3301469182@qq.com>
2026-05-12 20:10:33 +0800

    完成前端通用页面布局

---
+++ answerpass-frontend/src/router/routes.ts
@@ -0,0 +1,42 @@
+import { type RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+export const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () =>import("../views/AboutView.vue"),
+  },
+  {
+    path: "/hide",
+    name:"隐藏页面",
+    component: HomeView,
+    meta:{
+        hideInMenu:true,
+    }
+  },
+  {
+    path: "/user",
+    name: "用户",
+    meta:{
+      hideInMenu:true
+    },
+    component: () =>import("../views/user/UserLayout.vue"),
+    children:[
+      {
+        path:"/user/login",
+        name:"用户登录",
+        component: () =>import("../views/user/UserLoginView.vue"),
+      },
+      {
+        path:"/user/register",
+        name:"用户注册",
+        component: () =>import("../views/user/UserRegisterView.vue"),
+      }
+    ]
+  }
+];
